fe68f920af03529751500b62fc0a0adc00abe756
Kalu35 <luka.vuksanovic35@gmail.com>
2021-04-15 16:39:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import datetime as dt
+
+crypto = 'ETH'
+curr = 'EUR'
+
+start = dt.datetime(2016, 1, 1)
